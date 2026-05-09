e73f5b271dce280eed46e14f36d6ec82db58d132
IT24104357 <it24104357@my.sliit.lk>
2026-05-09 10:37:59 +0530

    Fix JWT middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,42 @@
 const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
-  const token = req.headers.authorization;
 
-  // ❌ No token → BLOCK
-  if (!token) {
-    return next();
+  const authHeader =
+    req.headers.authorization;
+
+  // NO TOKEN
+
+  if (!authHeader) {
+
+    return res.status(401).json({
+      msg: "No token provided",
+    });
+
   }
 
   try {
-    const decoded = jwt.verify(token, "mysecretkey");
+
+    // REMOVE "Bearer "
+
+    const token =
+      authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
     req.user = decoded;
+
     next();
+
   } catch (err) {
-    return res.status(401).json({ msg: "Invalid token" });
+
+    return res.status(401).json({
+      msg: "Invalid token",
+    });
+
   }
 };
 
